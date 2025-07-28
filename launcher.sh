079abeb6e41773b136556f1f41cbd902a68b38c5
hugo Fuentes <alverik@192.168.1.28>
2025-07-27 23:37:00 -0600

    Added new loger

--- launcher.sh
+++ launcher.sh
@@ -2,4 +2,4 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-java -jar $DIR/MuniesComparator-0.0.1-SNAPSHOT.jar
+java -jar $DIR/MuniesComparator-0.0.1-SNAPSHOT.jar 2>&1 | tee "$DIR/output.log"
--- src/main/java/com/example/muniescomparator/controller/FormateadorController.java
+++ src/main/java/com/example/muniescomparator/controller/FormateadorController.java
@@ -3,6 +3,7 @@ package com.example.muniescomparator.controller;
 import com.example.muniescomparator.file.CvsFileWriter;
 import com.example.muniescomparator.file.FileWriter;
 import com.example.muniescomparator.format.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/formater")
+@Slf4j
 public class FormateadorController {
 
     @GetMapping("/")
@@ -30,26 +32,22 @@ public class FormateadorController {
     public ResponseEntity<Resource> uploadFile(Model model, @RequestParam("file") MultipartFile file, @RequestParam String format,
                                                @RequestParam String fileName, @RequestParam String positions) throws Exception {
 
+        log.info("Iniciando formateador de archivo: {}", fileName);
+        log.info("Iniciando formateador de tipo: {}", format);
+
         FileFormat fileFormat = FileFormat.valueOf(format.toUpperCase());
         InputStream stream = file.getInputStream();
         fileName = fileName.substring(0, fileName.lastIndexOf('.'));
         String output =fileName;
         String convertedFile = null;
         FileWriter cvsFileWriter = new CvsFileWriter();
-        switch (fileFormat) {
-            case UVRA_ACEPTADAS:
-                convertedFile = new AceptedFormatter(cvsFileWriter).format(stream, output);
-                break;
-            case UVRA_RECHAZADAS:
-                convertedFile = new RejectedFormater(cvsFileWriter).format(stream, output);
-                break;
-            case S274:
-                convertedFile = new S274Formater(cvsFileWriter).format(stream, output);
-                break;
-            case POSITIONS:
-                convertedFile = new PositionalFormater(positions, cvsFileWriter).format(stream, output);
-                break;
-        }
+        convertedFile = switch (fileFormat) {
+            case UVRA_ACEPTADAS -> new AceptedFormatter(cvsFileWriter).format(stream, output);
+            case UVRA_RECHAZADAS -> new RejectedFormater(cvsFileWriter).format(stream, output);
+            case S274 -> new S274Formater(cvsFileWriter).format(stream, output);
+            case POSITIONS -> new PositionalFormater(positions, cvsFileWriter).format(stream, output);
+        };
+        log.info("Formato completo del archivo: {}", convertedFile);
         Path path = Paths.get("/tmp/"+ convertedFile );
         Resource resource = new InputStreamResource(Files.newInputStream(path));
         return ResponseEntity.ok()
